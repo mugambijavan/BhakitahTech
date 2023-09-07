f2aa89c24a200d29cad33480ca3b4979f4d414d2
Mugambi Javan <mugambijavan@gmail.com>
2023-09-07 22:15:42 +0300

    contact

--- src/components/Footer/Footer.js
+++ src/components/Footer/Footer.js
@@ -10,7 +10,7 @@ const Footer = () => {
       <LinkList>
         <LinkColumn>
           <LinkTitle>Call</LinkTitle>
-          <LinkItem href="tel:314-343-3432">+254 727 761 646</LinkItem>
+          <LinkItem href="tel:+254727761646">+254 727 761 646</LinkItem>
         </LinkColumn>
         <LinkColumn>
           <LinkTitle>Email</LinkTitle>
