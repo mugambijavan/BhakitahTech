eff9a8974a188771e2c0826c9b4b73225e7b48f9
Javoh <117713938+mugambijavan@users.noreply.github.com>
2023-12-07 20:16:05 +0300

    Update Projects.js

--- src/components/Projects/Projects.js
+++ src/components/Projects/Projects.js
@@ -5,6 +5,13 @@ const Projects = () => (
   <Section nopadding id="projects">
     <SectionDivider />
     <SectionTitle main>Your Vision, Our Innovation</SectionTitle>
+  <iframe
+      src="https://projects-exhibit.vercel.app/"
+      title="Projects Exhibit"
+      width="100%"
+      height="500px"
+      frameBorder="0"
+    ></iframe>
   </Section>
 );
 
