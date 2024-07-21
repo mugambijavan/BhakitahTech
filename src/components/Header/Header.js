8738f42a50c20a8103f6026acf60803dc9a6f6ce
Javoh <117713938+mugambijavan@users.noreply.github.com>
2024-07-21 19:46:42 +0300

    Target Links

--- src/components/Header/Header.js
+++ src/components/Header/Header.js
@@ -3,30 +3,28 @@ import React from 'react';
 import { AiFillGithub, AiFillInstagram, AiFillLinkedin } from 'react-icons/ai';
 import { DiCssdeck } from 'react-icons/di';
 
-import { Container, Div1, Div2, Div3, NavLink, SocialIcons } from './HeaderStyles';
+import { Container, Div1, Div3, SocialIcons } from './HeaderStyles';
 
-const Header = () =>  (
+const Header = () => (
   <Container>
     <Div1>
-      <Link href="/" style={{ display: 'flex', alignItems: 'center', color:"white" }}>
-
+      <Link href="/" style={{ display: 'flex', alignItems: 'center', color: 'white' }}>
         <DiCssdeck size="3rem" /> <span>BhakitahTech</span>
-
       </Link>
     </Div1>
-    
-      <Div3>
-      <SocialIcons href="https://github.com/mugambijavan">
-            <AiFillGithub size="3rem" />
-          </SocialIcons>
-          <SocialIcons href="https://www.linkedin.com/in/javan-mugambi-2351a81b0/">
-            <AiFillLinkedin size="3rem" />
-          </SocialIcons>
-          <SocialIcons href="https://www.instagram.com/_javoh._/">
-            <AiFillInstagram size="3rem" />
-          </SocialIcons>
-      </Div3>
-    </Container>
+
+    <Div3>
+      <SocialIcons href="https://github.com/mugambijavan" target="_blank" rel="noopener noreferrer">
+        <AiFillGithub size="3rem" />
+      </SocialIcons>
+      <SocialIcons href="https://www.linkedin.com/in/javan-mugambi-2351a81b0/" target="_blank" rel="noopener noreferrer">
+        <AiFillLinkedin size="3rem" />
+      </SocialIcons>
+      <SocialIcons href="https://www.instagram.com/_javoh._/" target="_blank" rel="noopener noreferrer">
+        <AiFillInstagram size="3rem" />
+      </SocialIcons>
+    </Div3>
+  </Container>
 );
 
 export default Header;
